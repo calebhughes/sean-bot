5488346950f5a65d99fb12313c0f0d3efc0335c7
Caleb Hughes <josh.caleb.hughes@gmail.com>
2020-11-02 21:35:25 -0500

    move commands out to cog

---
+++ cogs/headass.py
@@ -0,0 +1,50 @@
+import discord
+import csv
+import random
+from datetime import datetime
+from discord.ext import commands
+
+class Headass(commands.Cog):
+
+  def __init__(self, client):
+    self.client = client
+    self.filename = "headass_quotes.csv"
+  
+  @commands.command()
+  async def addquote(self, ctx, member: discord.Member = None, *, quote):
+    if member is None or quote is None:
+      await ctx.send('You must include a valid member and a quote for this command')
+    else:
+      self.save_quote(member, quote)
+
+  def save_quote(self, member: discord.Member, quote):
+    with open(self.filename, 'a+', newline='') as csvfile:
+      writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+      writer.writerow([member.id, quote, datetime.now()])
+
+  @commands.command()
+  async def headass(self, ctx):
+    member, quote, when = self.get_quote()
+    if member is None:
+      await ctx.send(quote)
+    else:
+      when_dt = datetime.strptime(when, '%Y-%m-%d %H:%M:%S.%f')
+      formatted_time = when_dt.strftime('%m-%d-%Y')
+      await ctx.send(f'<@{member}> on {formatted_time} "{quote}"')
+
+  def get_quote(self):
+    with open(self.filename, 'r+') as f:
+      reader = csv.reader(f, delimiter=',')
+      lines = list(reader)
+      if not lines:
+        return None,'No quotes added yet',None
+      else:
+        return random.choice(lines)
+
+def setup(client):
+  client.add_cog(Headass(client))
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,52 +1,60 @@
 #!/usr/bin/env python3
-
 import discord
 import re
-import headass
 import os
-
-from dateutil import tz
+from discord.ext import commands
 from dotenv import load_dotenv
 
 # Load the environment file that holds our token
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_API_TOKEN")
 
-client = discord.Client()
+client = commands.Bot(command_prefix=';')
 prog = re.compile('(\s+69\s+)|(^69\s+)|(^69$)|(.*\s+69.*)') # 69 regex blaze it
 
-def format_time(utc):
-    from_zone = tz.tzutc()
-    to_zone = tz.tzlocal()
-    updated_time = utc.replace(tzinfo=from_zone)
-    return updated_time.astimezone(to_zone)
 
-async def delete_user_last_message(invoke_message):
-    async for m in invoke_message.channel.history(limit=20):
-        if m.author == invoke_message.author and m.id != invoke_message.id:
-            await m.delete()
-            await invoke_message.delete()
-            break
 
+# async def delete_user_last_message(invoke_message):
+#     async for m in invoke_message.channel.history(limit=20):
+#         if m.author == invoke_message.author and m.id != invoke_message.id:
+#             await m.delete()
+#             await invoke_message.delete()
+#             break
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     await client.change_presence(status=discord.Status.online, activity=discord.Game('Fuck Sean'))
 
-@client.event
-async def on_message(message):
-    if prog.match(message.content):
-        await message.channel.send('nice')
-    elif 'nice' == message.content.lower() and not message.author.bot:
-        await message.channel.send('super nice')
-    elif message.content.startswith(';del'):
-        await delete_user_last_message(message)
-    elif message.content.startswith(';fucksean'):
-        await message.channel.send('fuck sean all my homies hate sean')
-    elif message.content.startswith(';headass'):
-        result = headass.parse_command(message)
-        if result != '':
-            await message.channel.send(result)
+@client.command(aliases=['del'])
+async def delete_last_message(context):
+    pass
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+# @client.event
+# async def on_message(message):
+#     if prog.match(message.content):
+#         await message.channel.send('nice')
+#     elif 'nice' == message.content.lower() and not message.author.bot:
+#         await message.channel.send('super nice')
+#     elif message.content.startswith(';del'):
+#         await delete_user_last_message(message)
+#     elif message.content.startswith(';fucksean'):
+#         await message.channel.send('fuck sean all my homies hate sean')
+#     elif message.content.startswith(';headass'):
+#         result = headass.parse_command(message)
+#         if result != '':
+#             await message.channel.send(result)
 
 client.run(DISCORD_TOKEN)
