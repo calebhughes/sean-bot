dae013f920cf6b531eb9e944490df31b56a6c2a4
Caleb Hughes <josh.caleb.hughes@gmail.com>
2020-11-04 03:49:03 -0500

    add diss cog and prefix command

---
+++ cogs/diss.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+class Diss(commands.Cog):
+
+  def __init__(self, client):
+    self.client = client
+    self.default_user_to_roast = 'Sean'
+
+  @commands.command()
+  async def diss(self, ctx, member: discord.Member = None):
+    if member is None or quote is None:
+      await ctx.send(f'fuck {self.default_user_to_roast} all my homies hate {self.default_user_to_roast}')
+    else:
+      await ctx.send(f'fuck <@{member.id}> all my homies hate {member.display_name}')
--- main.py
+++ main.py
@@ -12,27 +12,21 @@ DISCORD_TOKEN = os.getenv("DISCORD_API_TOKEN")
 client = commands.Bot(command_prefix=';')
 prog = re.compile('(\s+69\s+)|(^69\s+)|(^69$)|(.*\s+69.*)') # 69 regex blaze it
 
-
-
-# async def delete_user_last_message(invoke_message):
-#     async for m in invoke_message.channel.history(limit=20):
-#         if m.author == invoke_message.author and m.id != invoke_message.id:
-#             await m.delete()
-#             await invoke_message.delete()
-#             break
-
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     await client.change_presence(status=discord.Status.online, activity=discord.Game('Fuck Sean'))
 
-@client.command(aliases=['del'])
-async def delete_last_message(context):
-    pass
-
 @client.command()
 async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
+
+@client.command()
+async def prefix(ctx, new_prefix):
+    if new_prefix is None:
+        await ctx.send('You must include a new prefix to set')
+    else:
+        client.command_prefix = new_prefix
 
 @client.command()
 async def unload(ctx, extension):
@@ -41,20 +35,5 @@ async def unload(ctx, extension):
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
-
-# @client.event
-# async def on_message(message):
-#     if prog.match(message.content):
-#         await message.channel.send('nice')
-#     elif 'nice' == message.content.lower() and not message.author.bot:
-#         await message.channel.send('super nice')
-#     elif message.content.startswith(';del'):
-#         await delete_user_last_message(message)
-#     elif message.content.startswith(';fucksean'):
-#         await message.channel.send('fuck sean all my homies hate sean')
-#     elif message.content.startswith(';headass'):
-#         result = headass.parse_command(message)
-#         if result != '':
-#             await message.channel.send(result)
 
 client.run(DISCORD_TOKEN)
